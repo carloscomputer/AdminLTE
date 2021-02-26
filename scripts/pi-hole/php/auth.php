c277c18036ef67d74c106458c3f281e4270feab3
carloscomputer <post@carlos.berlin>
2021-02-26 12:26:35 +0100

    add nginx
    light wight first

--- scripts/pi-hole/php/auth.php
+++ scripts/pi-hole/php/auth.php
@@ -10,6 +10,7 @@ require_once('func.php');
 $ERRORLOG = getenv('PHP_ERROR_LOG');
 if (empty($ERRORLOG)) {
     $ERRORLOG = '/var/log/lighttpd/error.log';
+	$ERRORLOG = '/var/log/nginx/error.log';
 
     if (!file_exists($ERRORLOG) || !is_writable($ERRORLOG)) {
 	    $ERRORLOG = '/var/log/apache2/error.log';
