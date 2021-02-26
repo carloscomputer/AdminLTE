acfc49163a38f9215464a8a2a230f5b68eea539f
carloscomputer <post@carlos.berlin>
2021-02-26 12:04:06 +0100

    Update auth.php
    changes for nginx

--- scripts/pi-hole/php/auth.php
+++ scripts/pi-hole/php/auth.php
@@ -10,14 +10,15 @@ require_once('func.php');
 $ERRORLOG = getenv('PHP_ERROR_LOG');
 if (empty($ERRORLOG)) {
     $ERRORLOG = '/var/log/lighttpd/error.log';
+	$ERRORLOG = '/var/log/nginx/error.log';
 
-    if (!file_exists($ERRORLOG) || !is_writable($ERRORLOG)) {
-	    $ERRORLOG = '/var/log/apache2/error.log';
+   	 if (!file_exists($ERRORLOG) || !is_writable($ERRORLOG)) {
+		    $ERRORLOG = '/var/log/apache2/error.log';
 
-	    if (!file_exists($ERRORLOG) || !is_writable($ERRORLOG)) {
-		    $ERRORLOG = '/tmp/pi-hole-error.log';
-	    }
-    }
+	 	   if (!file_exists($ERRORLOG) || !is_writable($ERRORLOG)) {
+			    $ERRORLOG = '/tmp/pi-hole-error.log';
+	   	 }
+  	  }
 }
 
 function pi_log($message) {
